dd62a5f1e7f11cfb9c17a7ecb30c509c0a36cdaf
Jared Blackburn <BlackJar72@yahoo.com>
2025-05-02 05:24:03 -0400

    Added more data to Worldspace scriptable object in preparation for working on chunk management

--- Scripts/World/Chunks/Worldspace.cs
+++ Scripts/World/Chunks/Worldspace.cs
@@ -8,14 +8,24 @@ namespace kfutils.rpg {
     [CreateAssetMenu(menuName = "KF-RPG/World/World Space", fileName = "WorldSpace", order = 10)]
     public class Worldspace : ScriptableObject {
 
+        [Tooltip("The location of the scene file.")]
         [SerializeField] string scenePath;
+        [Tooltip("The Y coordinate below which is considered underwater; used for visual effect and switching movement to swim mode.")]
         [SerializeField] float seaLevel;
         [SerializeField] TransformData defaultStartLocation;
+        [Tooltip("The size of the chunk along the X and Z dimension (which should always be the same); should usually be a power of 2.")]
+        [SerializeField] int chunkSize = 128;
+        [Tooltip("The transform position of the first chunk, chunk[0,0]")]
+        [SerializeField] Vector2 chunkOffset;
 
 
         public string ScenePath => scenePath;
         public float SeaLevel => seaLevel;
         public TransformData DefaultStart => defaultStartLocation;
+        public int ChunkSize => chunkSize;
+        public Vector2 ChunkOffset => chunkOffset;
+        public float ChunkOffsetX => chunkOffset.x;
+        public float ChunkOffsetZ => chunkOffset.y;
 
 
         /// <summary>
