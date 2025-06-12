7614122d076ca670400e191cbd57bef9a61e6d66
Jared Blackburn <BlackJar72@yahoo.com>
2025-06-12 13:12:36 -0400

    Stuff

--- Scripts/Entity/EntityMoving.cs
+++ Scripts/Entity/EntityMoving.cs
@@ -6,25 +6,30 @@ namespace kfutils.rpg {
 
 
     [RequireComponent(typeof(NavMeshAgent))]
-    public class EntityMoving : EntityLiving {
+    public class EntityMoving : EntityLiving
+    {
 
         // This is to make sure this is never overriden into something harmful.
         protected sealed override void MakePC(string id) { base.MakePC(ID); }
 
-        public NavMeshAgent navMeshAgent;
+        public NavMeshAgent navAgent;
 
-         // Start is called once before the first execution of Update after the MonoBehaviour is created
-        protected override void Start() {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        protected override void Start()
+        {
             base.Start();
-            navMeshAgent = GetComponent<NavMeshAgent>();            
+            navAgent = GetComponent<NavMeshAgent>();
         }
-        
+
 
         // Update is called once per frame
         /*protected override void Update()
         {
             
         }*/
+
+
+        
 
 
 
