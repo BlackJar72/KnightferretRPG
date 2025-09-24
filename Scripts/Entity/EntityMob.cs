a1f23f6de4a945eaf75fe4633c2525a162782921
Jared Blackburn <BlackJar72@yahoo.com>
2025-09-24 14:18:21 -0400

    Started setup for utility AI systems

--- Scripts/Entity/EntityMob.cs
+++ Scripts/Entity/EntityMob.cs
@@ -1,10 +1,18 @@
+using System;
+using Animancer;
 using UnityEngine;
 
 
 namespace kfutils.rpg
 {
 
-    public class EntityMob : EntityActing
+    /// <summary>
+    /// An alternative to EntityActing for non-humanoid creatures
+    /// 
+    /// TODO!!!
+    /// 
+    /// </summary>
+    public class EntityMob : EntityMoving, ICombatantAI
     {
 
 
@@ -19,9 +27,17 @@ namespace kfutils.rpg
         protected float nextIdleTalk;
         protected float meleeStopDistance;
 
+        public bool IsBlocking => throw new NotImplementedException();
+
+        public AnimancerLayer ActionLayer => throw new NotImplementedException();
+
+        public AnimancerState ActionState => throw new NotImplementedException();
+
+        public CharacterInventory CharInventory => throw new NotImplementedException();
 
 
-#region  Overridden Unity Methods
+
+        #region  Overridden Unity Methods
 
         protected override void Awake()
         {
@@ -53,10 +69,124 @@ namespace kfutils.rpg
             base.OnDisable();
         }
 
-#endregion
+        #endregion
 
 
-        
+        public void MeleeAttack(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void DrawWeapon(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void SheatheWeapon(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void SwitchWeapon(IWeapon currentWeapon, IWeapon newWeapon)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void RangedAttack(IWeapon weapon, Vector3 direction)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Block(ItemEquipt item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void EndBlock(ItemEquipt item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void BlockDamage(Damages damage, BlockArea blockArea)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void BlockDamage(DamageData damage, BlockArea blockArea)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public BlockArea GetBlockArea()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void EquiptItem(ItemStack item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void UnequiptItem(ItemStack item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void UnequiptItem(EEquiptSlot slot)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public AnimancerState PlayAction(AvatarMask mask, ITransition animation, float time = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public AnimancerState PlayAction(AvatarMask mask, ITransition animation, Action onEnd, float time = 0, float delay = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void StopAction()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void RemoveEquiptAnimation()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void GetAimParams(out AimParams aim)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void PreSaveEquipt()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
 
 
 
---
+++ Scripts/Interfaces/IActivityObject.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+
+namespace kfutils.rpg
+{
+
+    /// <summary>
+    /// The object of an activity, where the character doing the activity is the subject. 
+    /// This could be an (usually inteactable) object in the world, and inventory item, 
+    /// a spell or similar ability, another character, or even nothing (effective, the 
+    /// subject is the object).  Thus, it needs to be an interface as different objects 
+    /// types may need to be handled differently. 
+    /// </summary>
+    public interface IActivityObject
+    {
+        public float GetUtility(EntityMoving entity); // This should probably table the subject as a parameter(?) 
+
+
+
+    }
+
+}
