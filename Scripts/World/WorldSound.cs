5b1ae29c22b7bb71917df4a15b3659faf1177a1d
Jared Blackburn <BlackJar72@yahoo.com>
2025-11-13 14:00:47 -0500

    More work on sounds

--- Scripts/World/WorldSound.cs
+++ Scripts/World/WorldSound.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 namespace kfutils.rpg {
 
+    public enum SoundType
+    {
+        General = 0,  // Most sounds, including player movement and attacks 
+        Alert = 1,    // Alerts from faction members (could include alarms too) 
+        Pain = 2,     // Pain sounds from creatures, who may make them even while dying
+        Speech = 3,   // Directed speech, especially talking to the hearer
+        Music = 4,    // Music, you know what it is
+        Strange = 5,  // Weird sounds that might leave someone wondering what it is
+        Dramatic = 6  // Loud, dangerous, or otherwise attention grabbing sounds  
+    }
+
+
+    public interface ISoundSource
+    {
+        public void MakeSound(float loudness, SoundType soundType);
+    }
+
 
     /// <summary>
     /// This represents a sound in world, which may be heard by NPCs (including enemies). 
@@ -15,22 +32,30 @@ namespace kfutils.rpg {
     {
         private Vector3 location;
         private float range; // The range at which it may be heard 
+        private SoundType type;
+        private ISoundSource source; // The game object the sound came from
 
         public Vector3 Location => location;
         public float Loudness => range;
+        public SoundType Type => type;
+        public ISoundSource Source => source;
 
 
-        public WorldSound(Vector3 position, float loudness)
+        public WorldSound(Vector3 position, float loudness, SoundType soundType = SoundType.General, ISoundSource src = null)
         {
             location = position;
             range = loudness;
+            type = soundType;
+            source = src;
         }
 
 
-        public WorldSound(Transform transform, float loudness)
+        public WorldSound(Transform transform, float loudness, SoundType soundType = SoundType.General, ISoundSource src = null)
         {
             location = transform.position;
             range = loudness;
+            type = soundType;
+            source = src;
         }
 
     }
