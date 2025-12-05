009e9a289f3e4042adefe4dd09618b8917c65192
Jared Blackburn <BlackJar72@yahoo.com>
2025-12-05 06:31:30 -0500

    Added callback for character creation extensions

--- Scripts/UI/StartUI/CharacterCreationUI.cs
+++ Scripts/UI/StartUI/CharacterCreationUI.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
+using System.Collections;
 
 namespace kfutils.rpg {
 
@@ -11,6 +13,23 @@ namespace kfutils.rpg {
         [SerializeField] StatCreationUI statCreationUI;
         [SerializeField] Button confrimButton;
         [SerializeField] GameObject nameWarning;
+
+#region External Control
+        public delegate void CreatedCharacter();
+        public static event CreatedCharacter CharacterCreationEvent;
+        
+        private static bool characterCreated;
+        public static bool CharacterCreated => characterCreated;
+        /// <summary>
+        /// Call this in any method subscribed to CharacterCreationEvent at which character creation 
+        /// might fail or require further action, to tell if the external requirements have been met. 
+        /// If any requirement has failed, this should stop the game from proceeding to starting 
+        /// gameplay. In addition, a couple frames of delay will be provided for such methods to 
+        /// run, do what they need to do, and return their result.
+        /// </summary>
+        /// <param name="success"></param>
+        public static void SetCreationSuccess(bool success) => characterCreated &= success;
+#endregion External Control
 
  
 
@@ -23,6 +42,7 @@ namespace kfutils.rpg {
         public void StartNewCharacter()
         {
             Time.timeScale = 0.0f;
+            characterCreated = false;
             statCreationUI.StartNewCharacter();
             charcterName.text = "";
         }
@@ -43,8 +63,18 @@ namespace kfutils.rpg {
             }
             else 
             {
-                StartGame();
+                characterCreated = true;
+                CharacterCreationEvent?.Invoke();
+                StartCoroutine(StartIfCharacterCreated());
             }
+        }
+
+
+        private IEnumerator StartIfCharacterCreated()
+        {
+            yield return null;
+            yield return null;
+            if(characterCreated) StartGame();
         }
 
 
